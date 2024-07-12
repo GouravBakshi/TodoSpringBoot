1f67dfe5eb6b324943414b1989928d1b2fbedccf
GouravBakshi <96644786+GouravBakshi@users.noreply.github.com>
2024-07-12 17:22:05 +0530

    Add files via upload

---
+++ Backened/src/main/java/com/gourav/todo/TodoManagementApplication.java
@@ -0,0 +1,31 @@
+package com.gourav.todo;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class TodoManagementApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagementApplication.class, args);
+	}
+
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/api/**")
+//						.allowedMethods("*")
+//						.allowedOrigins("http://localhost:3000");
+//			}
+//		};
+//	}
+}
